e1870f364306a35913302fae406b8ec14a65f66f
Marko Schulz <marko.schulz@it-agile.de>
2010-04-15 12:54:13 +0200

    Kata Durchlauf X+1, abgebrochen

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,30 +1,26 @@
 
-describe 'roman()'
-	it 'computes one digit romans'
-		roman("I").should.be 1
-		roman("V").should.be 5
-		roman("X").should.be 10
-		roman("L").should.be 50
-		roman("C").should.be 100
-		roman("D").should.be 500
-		roman("M").should.be 1000
+describe 'roman'
+    it 'should compute single roman digits'
+      roman("I").should.be 1
+      roman("V").should.be 5
+      roman("X").should.be 10
+      roman("L").should.be 50
+      roman("C").should.be 100
+    end
+
+	it 'should compute multi digit romans with the same digits'
+		roman("II").should.be 2
+		roman("XX").should.be 20
+		roman("CCC").should.be 300
 	end
 	
-	it 'computes simple multi digit romans'
-		roman("II").should.be 2
-		roman("CCX").should.be 210
+	it 'should compute compute mixed digit romans'
+		roman("VI").should.be 6
+		roman("CCXVII").should.be 217
 	end
 
-	it 'computes multi digit romans including substractions'
+	it 'should compute romans with subtractions'
 		roman("IV").should.be 4
 		roman("XIX").should.be 19
-		roman("XL").should.be 40
-		roman("XC").should.be 90
-		roman("CD").should.be 400
-		roman("CM").should.be 900
-	end
-	
-	it 'computes my birthyear and then I\'m done'
-		roman("MCMLXXII").should.be 1972
 	end
 end
