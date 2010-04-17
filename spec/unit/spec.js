30e62c36a8da8cfe64d0740cb3100b6ba74c360f
Marko Schulz <marko.schulz@it-agile.de>
2010-04-17 21:23:44 +0200

    Kata Durchlauf X+2

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,26 +1,33 @@
-
-describe 'roman'
-    it 'should compute single roman digits'
-      roman("I").should.be 1
-      roman("V").should.be 5
-      roman("X").should.be 10
-      roman("L").should.be 50
-      roman("C").should.be 100
-    end
-
-	it 'should compute multi digit romans with the same digits'
-		roman("II").should.be 2
-		roman("XX").should.be 20
-		roman("CCC").should.be 300
+describe 'roman()' 
+	it 'should compute single digit romans'
+		roman("I").should.be 1
+		roman("V").should.be 5
+		roman("X").should.be 10
+		roman("L").should.be 50
+		roman("C").should.be 100
+		roman("D").should.be 500
+		roman("M").should.be 1000
 	end
 	
-	it 'should compute compute mixed digit romans'
+	it 'should compute multi digit romans with the same digit'
+	 	roman("II").should.be 2
+		roman("XXX").should.be 30
+	end
+	
+	it 'should compute multi digit romans with mixed digits'
 		roman("VI").should.be 6
-		roman("CCXVII").should.be 217
+		roman("XXIII").should.be 23
 	end
-
-	it 'should compute romans with subtractions'
+	
+	it 'should compute all subtractions'
 		roman("IV").should.be 4
-		roman("XIX").should.be 19
+		roman("MCMXC").should.be 1990
+		roman("CMXCIX").should.be 999
+		roman("CDXLIV").should.be 444
 	end
+	
+	it 'should compute some meaningless year'
+		roman("MCMLXXII").should.be 1972
+	end
+	 
 end
