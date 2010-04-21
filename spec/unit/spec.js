cd18a41a2b2f07182c04c7406365115f48f7b677
Marko Schulz <marko.schulz@it-agile.de>
2010-04-22 01:51:38 +0200

    Kata Durchlauf X+7

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,39 +1,29 @@
 describe 'roman()'
-	var testcases = {
-		I: 1,
-		II: 2,
-		III: 3
-	}
-
-	for (testcase in testcases) {
-		JSpec.context = { input: testcase, expected: testcases[testcase] };
-
-		it 'should '+testcase+' '+testcases[testcase]
-		  roman(input).should.be expected
-		end
-	};
-
-
 	it 'should compute single digit romans'
-	  roman('I').should.be 1
-	  roman('V').should.be 5
-	  roman('X').should.be 10
-	  roman('L').should.be 50
-	  roman('C').should.be 100
-	  roman('D').should.be 500
-	  roman('M').should.be 1000
+		roman('I').should.be 1
+		roman('V').should.be 5
+		roman('X').should.be 10
+		roman('L').should.be 50
+		roman('C').should.be 100
+		roman('D').should.be 500
+		roman('M').should.be 1000
 	end
 	
-	it 'should compute simple multi digit romans'
-	  roman('II').should.be 2
-	  roman('CX').should.be 110
-	  roman('XVI').should.be 16
+	it 'should compute romans with additive digits'
+		roman('II').should.be 2
+		roman('CX').should.be 110
+		roman('XVI').should.be 16		
 	end
 	
 	it 'should compute romans with subtractions'
-	  roman('IV').should.be 4
-	  roman('MCMXC').should.be 1990
-	  roman('CDXLIV').should.be 444
-	  roman('CMXCIX').should.be 999
+		roman('IV').should.be 4
+		roman('XIV').should.be 14
+		roman('XL').should.be 40
+		roman('CDXLIV').should.be 444
+		roman('CMXCIX').should.be 999
+	end
+	
+	it 'should compute some meaningless year'
+		roman('MCMLXXII').should.be 1972
 	end
 end
